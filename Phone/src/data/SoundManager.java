4e03b8b8a69a4dd209d800a02b2abd0dfef7ead2
TMathMeyer@gmail.com <TMathMeyer@gmail.com@04549ebc-61f3-c9a6-f139-71ba97a731b0>
2012-02-25 18:55:28 +0000

    feb. 25 2012
    last commit before a build and test

--- Phone/src/data/SoundManager.java
+++ Phone/src/data/SoundManager.java
@@ -1,5 +1,33 @@
 package data;
 
-public class SoundManager {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class SoundManager {
+	
+	private PrintWriter out;
+	SpeakerOutput speakerOutput;
+	
+	public SoundManager(Socket s){
+		
+		try {
+            out = new PrintWriter(s.getOutputStream(), true);
+        }catch(IOException iE) {}
+		
+		Thread soundReceiver = new Thread(new SoundReceiver(s, this));
+		soundReceiver.start();
+		
+		Thread micIn = new Thread(new MicrophoneInput(this));
+		micIn.start();
+		
+		speakerOutput = new SpeakerOutput();
+	}
+	
+	public void closeOutStream() {
+        if(out != null)out.close();
+    }
+    public void sendAway(String s)  {
+        if(out !=null)out.println(s);
+    }
 }
--- Phone/src/data/SpeakerOutput.java
+++ Phone/src/data/SpeakerOutput.java
@@ -1,5 +1,40 @@
 package data;
 
-public class SpeakerOutput {
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+
+public class SpeakerOutput {
+	
+	public void playAudio(byte[] soundByte){
+		try{
+			InputStream byteArrayInputStream = new ByteArrayInputStream(soundByte);
+			AudioFormat audioFormat = this.getAudioFormat();
+			AudioInputStream audioInputStream = new AudioInputStream(byteArrayInputStream, audioFormat, soundByte.length/audioFormat.getFrameSize());
+			DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
+			SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+			sourceDataLine.open(audioFormat);
+		    sourceDataLine.start();
+			int cnt;
+	    	while((cnt = audioInputStream. read(soundByte, 0, soundByte.length)) != -1)
+	    		if(cnt > 0)sourceDataLine.write(soundByte, 0, cnt);
+	    	sourceDataLine.drain();
+	    	sourceDataLine.close();
+		}
+		catch(Exception s){}
+	}
+	
+	private AudioFormat getAudioFormat(){
+	    float sampleRate = 8000.0F;
+	    int sampleSizeInBits = 16;
+	    int channels = 1;
+	    boolean signed = true;
+	    boolean bigEndian = false;
+	    return new AudioFormat(sampleRate,sampleSizeInBits,channels,signed,bigEndian);
+	}
 }
